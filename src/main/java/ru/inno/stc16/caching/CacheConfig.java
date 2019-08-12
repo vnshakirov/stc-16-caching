4cc0aaf09237a5fc578c07bb33ca53b406c7c543
Vadim Shakirov <vadim.shakirov@rtlabs.ru>
2019-08-12 10:43:05 +0300

    JSR-107 EhCache

--- src/main/java/ru/inno/stc16/caching/CacheConfig.java
+++ src/main/java/ru/inno/stc16/caching/CacheConfig.java
@@ -2,9 +2,11 @@ package ru.inno.stc16.caching;
 
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.cache.ehcache.EhCacheCacheManager;
+import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 @Configuration
 @EnableCaching
@@ -12,6 +14,14 @@ public class CacheConfig {
 
     @Bean
     public CacheManager cacheManager() {
-        return new ConcurrentMapCacheManager();
+        return new EhCacheCacheManager(ehCacheCacheManager().getObject());
+    }
+
+    @Bean
+    public EhCacheManagerFactoryBean ehCacheCacheManager() {
+        EhCacheManagerFactoryBean cmfb = new EhCacheManagerFactoryBean();
+        cmfb.setConfigLocation(new ClassPathResource("ehcache.xml"));
+        cmfb.setShared(true);
+        return cmfb;
     }
 }
