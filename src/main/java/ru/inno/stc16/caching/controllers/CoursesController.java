33dc39e4de7cddc0704bb190419312333bbb4373
Vadim Shakirov <vadim.shakirov@rtlabs.ru>
2020-03-02 17:09:53 +0300

    jcs

--- src/main/java/ru/inno/stc16/caching/controllers/CoursesController.java
+++ src/main/java/ru/inno/stc16/caching/controllers/CoursesController.java
@@ -4,6 +4,7 @@ import org.apache.commons.jcs.access.exception.CacheException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.inno.stc16.caching.CourseRepository;
 import ru.inno.stc16.caching.entity.Course;
@@ -11,17 +12,18 @@ import ru.inno.stc16.caching.entity.Course;
 import java.util.Collection;
 
 @RestController
+@RequestMapping("/api/v1/courses")
 public class CoursesController {
 
     @Autowired
     CourseRepository repository;
 
-    @GetMapping("/courses")
+    @GetMapping
     public Collection<Course> getAll() throws CacheException {
         return repository.getCourses();
     }
 
-    @GetMapping("/courses/{id}")
+    @GetMapping("/{id}")
     public Course getById(@PathVariable("id") Integer id) throws CacheException {
         return repository.getCourse(id);
     }
